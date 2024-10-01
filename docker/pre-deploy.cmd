578b7ab0e5f765d2a68f576588920bee798bdb41
Etienne LANGLET <elanglet@eni.fr>
2024-10-01 13:36:20 +0200

    Correction commande pre-deploy

--- docker/pre-deploy.cmd
+++ docker/pre-deploy.cmd
@@ -1,4 +1,4 @@
 mkdir .\docker\app\
-xcopy . .\docker\app\ /V /NFL /NDL /NJH /NJS /nc /ns /np /MIR /XF .gitignore /XD .git\ docker\ 
+robocopy . .\docker\app\ /V /NFL /NDL /NJH /NJS /nc /ns /np /MIR /XF .gitignore /XD .git\ docker\ 
 if %ERRORLEVEL% EQU 1 echo OKCOPY & goto end
 :end
